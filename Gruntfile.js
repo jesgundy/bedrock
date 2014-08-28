11708bc837cb5ac8fddd4c4fae13ac01cc993c06
Jes Gundy <jesse.gundy@gmail.com>
2014-08-28 00:23:18 -0400

    Added Kit templating, colorized bash output

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,6 +39,12 @@ module.exports = function(grunt) {
         files: 'scss/**/*.scss',
         tasks: ['sass'],
       },
+      kit: {
+        files: ['content/**/*.{kit,html}', 'index.kit'],
+        tasks: ['codekit'],
+      },
+    },
+
     imagemin: {
       img: {
         options: {
@@ -50,6 +56,13 @@ module.exports = function(grunt) {
         }]
       },
     },
+
+    codekit: {
+      index: {
+        files: {
+          'index.html' : 'index.kit',
+        }
+      },
     },
 
   }); // end config
@@ -61,20 +74,28 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-codekit');
 
 
 
   // Default grunt task
   grunt.registerTask('default', 'Log grunt options to screen.', function(){
+
+    function highlight(text) {
+      return '\033[0;36m'+ text +'\033[0m';
+    }
+
     // intro
     grunt.log.writeln('\n');
     grunt.log.writeln('BEDROCK: GRUNT TASKS');
-    grunt.log.writeln('--------------------------');
+    grunt.log.writeln('--------------------');
 
     // List out all of the options to run (automate this later?)\
-    grunt.log.writeln('*sass:* Compiles all styles in the scss/ folder.');
-    grunt.log.writeln('*jshint:* Runs JSHint on all component/ js files.');
-    grunt.log.writeln('*watch:* Watches for changes in scss/ folder.');
+    grunt.log.writeln( highlight('sass:')+ ' Compiles all styles in the scss/ folder');
+    grunt.log.writeln( highlight('codekit:')+ ' Compiles index.kit file into index.html');
+    grunt.log.writeln( highlight('watch:')+ ' Watches for changes in scss/ and content/ folders');
+    grunt.log.writeln( highlight('jshint:')+ ' Runs JSHint on all component/ js files');
+    grunt.log.writeln( highlight('imagemin:')+ ' Crushes all jpgs, pngs and gifs in the img/ folder');
 
     // space
     grunt.log.write('\n');
